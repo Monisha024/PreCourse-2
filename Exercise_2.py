a42e4762d247e08770949b5d55a85c831d651b63
Monisha R <36629408+Monisha20@users.noreply.github.com>
2024-12-21 22:51:05 -0600

    changes updated

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,17 +2,26 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
+    
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i, j)
+    swap(arr, i + 1, high)
+    return i + 1
   
-  
-    #write your code here
-  
-
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+    
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
-  
-# Driver code to test above 
+    if low < high:
+        x = partition(arr, low, high)
+        quickSort(arr, low, x - 1)
+        quickSort(arr, x + 1, high)
+
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
